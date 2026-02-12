8136dfe5caa1e707049548a1a8b12fa632a53441
Yann <yann@bolliger.io>
2026-02-12 17:41:38 +0100

    Add min max struct to local laws

--- local-laws/src/main.rs
+++ local-laws/src/main.rs
@@ -1,38 +1,41 @@
 #![no_main]
 
 use delta_domain_sdk::{
-    base::{
-        core::Planck,
-        verifiable::{
-            VerifiableType, VerifiableWithDiffs, VerificationContext,
-            debit_allowance::AllowanceAmount,
-        },
+    base::verifiable::{
+        VerifiableType, VerifiableWithDiffs, VerificationContext, debit_allowance::AllowanceAmount,
     },
     proving::local_laws::{LocalLaws, LocalLawsError},
 };
 
-const MAX_AMOUNT: Planck = 100;
+#[derive(serde::Serialize, serde::Deserialize)]
+struct Amounts {
+    pub min: u64,
+    pub max: u64,
+}
 
 /// A local law that only allows transfers up to a given amount
 #[derive(Debug, Copy, Clone, Default)]
-struct MaxAmountLaw {}
+struct MaxAmountLaw;
 
 impl LocalLaws for MaxAmountLaw {
-    type Input<'a> = ();
+    type Input<'a> = Amounts;
 
     fn validate<'a>(
         verifiables: &[VerifiableWithDiffs],
         _context: &VerificationContext,
-        _input: &(),
+        input: &Amounts,
     ) -> Result<(), LocalLawsError> {
+        let max_spending = input.max;
+        let min_spending = input.min;
+
         for verifiable in verifiables {
             if let VerifiableType::DebitAllowance(debit_allowance) = &verifiable.verifiable {
-                for amount in debit_allowance.payload().content().allowances.values() {
+                for (&token_kind, amount) in &debit_allowance.payload().content().allowances {
                     if let AllowanceAmount::Fungible(amount) = amount
-                        && *amount <= MAX_AMOUNT
+                        && (*amount > max_spending || *amount < min_spending)
                     {
                         return Err(LocalLawsError::new(format!(
-                            "Cannot spend more than {MAX_AMOUNT}"
+                            "Debit amount for token {token_kind} must be between {min_spending} and {max_spending}, was {amount}"
                         )));
                     }
                 }
@@ -42,13 +45,15 @@ impl LocalLaws for MaxAmountLaw {
     }
 }
 
+const AMOUNTS: Amounts = Amounts { min: 10, max: 100 };
+
 sp1_zkvm::entrypoint!(main);
 fn main() {
     // conform to the standard input format
     let verifiables = sp1_zkvm::io::read::<Vec<VerifiableWithDiffs>>();
     let verification_context = sp1_zkvm::io::read::<VerificationContext>();
 
-    MaxAmountLaw::validate(&verifiables, &verification_context, &())
+    MaxAmountLaw::validate(&verifiables, &verification_context, &AMOUNTS)
         .expect("The local laws are not satisfied by the verifiables.");
 
     // committing the inputs
